df3126227bfae1e859690cbffe495a2444c25ae9
Dimas Rehan Nabiel Avianto <dimasrehannabiel@gmail.com>
2022-04-20 15:05:40 +0700

    PerPergerakan Dasar Pemain
    Menambahkan fungsionalitas dari game objek pemain untuk bisa melakukan pergerakan maju, mundur, dan melompat

--- Shoot or Reminisce/Assets/Scripts/Pemain_Kontrol.cs
+++ Shoot or Reminisce/Assets/Scripts/Pemain_Kontrol.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Pemain_Kontrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController karakterKontroler;
+    public float kecepatanGerak = 12f;
+    public float gravitasi = -9.81f;
+    public float tinggiLompatan = 3f;
 
-    // Update is called once per frame
+    public Transform pengecekPlatform;
+    public float jarakPlatform = 0.4f;
+    public LayerMask maskPlatform;
+    bool diPlatform;
+
+    Vector3 velocity;
     void Update()
     {
-        
+       Pergerakan(); 
+    }
+
+    void Pergerakan()
+    {
+        diPlatform = Physics.CheckSphere(pengecekPlatform.position, jarakPlatform, maskPlatform);
+
+        if(diPlatform && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 pergerakan = transform.right * x + transform.forward * z;
+        karakterKontroler.Move(pergerakan * kecepatanGerak * Time.deltaTime);
+
+        Lompat();
+        TerapkanGravitasi();
+    }
+
+    void Lompat()
+    {
+        if(Input.GetButtonDown("Jump") && diPlatform)
+        {
+            velocity.y = Mathf.Sqrt(tinggiLompatan * -2f * gravitasi);
+        }
+
+    }
+
+    void TerapkanGravitasi()
+    {
+        velocity.y += gravitasi * Time.deltaTime;
+        karakterKontroler.Move(velocity * Time.deltaTime);
     }
 }
